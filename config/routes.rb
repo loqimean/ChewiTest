f2c3c9b18a9577b6d4181cc9447eb838ae1875fc
Hunter2065 <vanuha277@gmail.com>
2021-08-13 15:39:25 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do
+    collection do
+      post :search, to: "users/searches#index"
+    end
+  end
   root 'users#index'
   resources :users
 end
