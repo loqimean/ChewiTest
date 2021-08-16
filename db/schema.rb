7c7c2e65f5a601e4b1bd40504131035250c29861
Hunter2065 <vanuha277@gmail.com>
2021-08-16 12:36:13 +0300

    generated country migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_054704) do
+ActiveRecord::Schema.define(version: 2021_08_16_083123) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
