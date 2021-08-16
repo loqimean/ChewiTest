d5b21aa7ad0ed11b223e04aea13f9f3b8eec14a9
Hunter2065 <vanuha277@gmail.com>
2021-08-16 11:39:44 +0300

    initialize variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+    @searched_users = UsersIndex.query(query_string: { query: '' })
   end
 
   # GET /users/1 or /users/1.json
