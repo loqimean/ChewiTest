d07e50f3bc5450d1076f4a63cbfb350ffd8f495c
Hunter2065 <vanuha277@gmail.com>
2021-08-16 17:55:38 +0300

    updates for country data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ Chewy.strategy(:atomic) do
     user = User.new
     user.name = Faker::Name.first_name
     user.email = Faker::Internet.email
+    user.city = Faker::Address.city
     user.save!
   end
 end
