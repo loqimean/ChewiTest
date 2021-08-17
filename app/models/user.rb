4ae93185b3deeb68be2d3039005d7a821ad87717
Hunter2065 <vanuha277@gmail.com>
2021-08-17 16:15:19 +0300

    added relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  belongs_to :city
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  
+
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
