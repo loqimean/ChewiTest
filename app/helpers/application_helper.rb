23311c7d57c3fa519e2f5d37f33cff9fd91b94c7
Hunter2065 <vanuha277@gmail.com>
2021-08-17 19:49:38 +0300

    created method for statistic by aggs search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def clean_keys(model_obj)
+    array_of_values = model_obj.attributes.except('created_at',
+                                                  'updated_at').keys
+    array_of_values.delete_if { |element| element.include?('id') }
+  end
+
+  def get_statistic_count_by_aggs(object)
+    @cities_aggs = UsersSearch.new.search.aggs
+
+    @cities_aggs['cities']['buckets'].each do |hash|
+      if hash['key'] == object.id
+        return hash['doc_count']
+      end
+    end
+  end
 end
