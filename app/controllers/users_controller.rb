2664cc1663af92c1218cba1400ccdb657ce60ca2
Hunter2065 <vanuha277@gmail.com>
2021-08-18 18:48:24 +0300

    feature(users_controller.rb) use request cities by aggs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
-    @cities = City.all
+    @filters = ElasticAggregationsSerializer.new(collection.aggregations).to_hash
   end
 
   # GET /users/1 or /users/1.json
