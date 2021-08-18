d8bf3ee2a377735bda01967ad5e53b53453e5995
Hunter2065 <vanuha277@gmail.com>
2021-08-18 13:10:29 +0300

    test(user_spec.rb) update tests and some refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:test_user) { User.new(name: 'Nikolas', email: 'nik@mail.io') }
+  let(:test_city) { City.create!(name: 'Kyiv') }
+  let(:test_user) do
+    User.new(name: 'Nikolas', email: 'nik@mail.io', city: test_city)
+  end
 
   context 'whith' do
     it 'valid attributes' do
@@ -9,39 +12,53 @@ RSpec.describe User, type: :model do
     end
   end
 
-  context 'when not valid' do
-    it 'name presence' do
-      test_user.name = nil
-      expect(test_user).not_to be_valid
-    end
-
-    it 'name length too short' do
+  context 'when name' do
+    it 'length too short' do
       test_user.name = 'q'
       expect(test_user).not_to be_valid
     end
 
-    it 'name length too long' do
+    it 'length too long' do
       test_user.name = 'q' * 51
       expect(test_user).not_to be_valid
     end
+  end
 
-    it 'email presence' do
+  context 'without presence' do
+    it 'name not valid' do
+      test_user.name = nil
+      expect(test_user).not_to be_valid
+    end
+
+    it 'email not valid' do
       test_user.email = nil
       expect(test_user).not_to be_valid
     end
 
-    it 'email length too long' do
+    it 'city not valid' do
+      test_user.city = nil
+      expect(test_user).not_to be_valid
+    end
+  end
+
+  context 'when email' do
+    it 'already been taken' do
+      User.create(name: test_user.name, email: test_user.email, city: test_city)
+      expect(test_user).not_to be_valid
+    end
+
+    it 'length too long' do
       test_user.name = 'q' * 256
       expect(test_user).not_to be_valid
     end
 
-    it 'email format' do
-      test_user.email = 'test'
+    it 'length too short' do
+      test_user.email = 'q' * 4
       expect(test_user).not_to be_valid
     end
 
-    it 'email already been taken' do
-      User.create(name: test_user.name, email: test_user.email)
+    it 'has incorrect format' do
+      test_user.email = 'test'
       expect(test_user).not_to be_valid
     end
   end
