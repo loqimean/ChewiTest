9afca7f7ec9e5dea0ed6ff8429f35b8c7c7e3dd0
Hunter2065 <vanuha277@gmail.com>
2021-08-19 19:32:13 +0300

    feature(users_search.rb) add cities filter to search

--- app/searches/users_search.rb
+++ app/searches/users_search.rb
@@ -1,16 +1,18 @@
 class UsersSearch
   FILTER_SIZE = 1000
 
-  attr_reader :query
+  attr_reader :query, :filter
 
   def initialize(args = {})
     @query = args[:query]
+    @filter = args[:filter_cities]
   end
 
   def search
     chewy_index
-      .query(query_condition)
+      .query(query_condition).limit(FILTER_SIZE)
       .aggs(aggregation_condition)
+      .filter(filter_condition)
   end
 
   private
@@ -21,22 +23,43 @@ class UsersSearch
 
     def aggregation_condition
       {
-        cities: {
-          terms: {
-            field: "city_id",
-            size: FILTER_SIZE
+        filters: {
+          global: {},
+          aggs: {
+            cities: {
+              terms: {
+                field: "city_id",
+                size: FILTER_SIZE
+              }
+            }
           }
         }
       }
     end
 
     def query_condition
-      return {} if query.blank?
+      return {
+        match_all: {}
+      } if query.blank?
 
       {
         query_string: {
           fields: [:name, :email],
           query: query
+        }
+      }
+    end
+
+    def filter_condition
+      return {} if filter.blank?
+
+      {
+        bool: {
+          must: [
+            { terms: {
+              "city_id": filter
+            } }
+          ]
         }
       }
     end
