de3e8b1815b7975382919936717d274e15fb7cbe
Hunter2065 <vanuha277@gmail.com>
2021-08-25 10:03:31 +0300

    feature(app) add checking mechanizm for seniorities

--- app/javascript/packs/users/index.js
+++ app/javascript/packs/users/index.js
@@ -1,17 +1,28 @@
 const CITIES_FILTER_NAME = 'city_ids';
+const SENIORITIES_FILTER_NAME = 'seniorities';
 
 // For delete element from array using method filter
 let arrayRemove = (arr, value = '') => arr.filter(ele => ele != value);
+let generateLink = obj => {
+  let url = '?';
 
-// Get all links for cities filter
-let cityBoxesList = [...document.getElementsByClassName('cities_filter_items')];
-// Get cities filter status
+  for( const key in obj) {
+    url += (obj[key].length > 0 ? key + '=' + obj[key] + '&' : '');
+  }
+  return url
+}
+
+let cityBoxesList = [...document.getElementsByClassName('city_filter_items')];
+let seniorityBoxesList = [...document.getElementsByClassName('seniority_filter_items')];
 let citiesFilterStatus = arrayRemove(document.getElementById('cities-filter-status')
-                                      .getAttribute('data-cities-filter-status')
+                                      .getAttribute('data-filter-status')
                                       .split(','));
-// Create filters object for generating URL
+let senioritiesFilterStatus = arrayRemove(document.getElementById('seniorities-filter-status')
+                                      .getAttribute('data-filter-status')
+                                      .split(','));
 let filters = {
-  city_ids: citiesFilterStatus
+  city_ids: citiesFilterStatus,
+  seniorities: senioritiesFilterStatus
 }
 
 cityBoxesList.forEach(cityBox => {
@@ -33,6 +44,31 @@ cityBoxesList.forEach(cityBox => {
     }
 
     // Generate link with filter params
-    cityFilterLink.href = `?${CITIES_FILTER_NAME}=${filters[CITIES_FILTER_NAME]}`;
+    cityFilterLink.href = generateLink(filters);
+  });
+});
+
+
+
+seniorityBoxesList.forEach(seniorityBox => {
+  // Gets city link
+  let seniorityFilterLink = seniorityBox.parentElement;
+  // Gets city checkboxds
+  let seniorityCheckbox = seniorityBox.firstElementChild;
+  let seniorityName = seniorityCheckbox.value;
+
+  // Checks if filters was selected (for saving state after reload page)
+  seniorityCheckbox.checked = filters[SENIORITIES_FILTER_NAME].includes(seniorityName);
+
+  seniorityFilterLink.addEventListener('click', event => {
+    // Checks if was filter selected exists or no
+    if (filters[SENIORITIES_FILTER_NAME].includes(seniorityName)) {
+      filters[SENIORITIES_FILTER_NAME] = arrayRemove(filters[SENIORITIES_FILTER_NAME], seniorityName);
+    } else {
+      filters[SENIORITIES_FILTER_NAME].push(seniorityName);
+    }
+
+    // Generate link with filter params
+    seniorityFilterLink.href = generateLink(filters);
   });
 });
