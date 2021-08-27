a7fbeff56d7ee1c796f673c003f5b8137c2ee5cd
Hunter2065 <vanuha277@gmail.com>
2021-08-27 15:51:46 +0300

    feature(app/config) add localizing support for 'en' and 'ua'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    [params[:locale], extract_locale, I18n.default_locale].each do |l|
+      if l && I18n.available_locales.index(l.to_sym)
+        I18n.locale = l
+        break
+      end
+    end
+  end
+
+  def extract_locale
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first if request.env['HTTP_ACCEPT_LANGUAGE']
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ChuwiTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    # Configure i18n for localizing app
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %i[uk en]
 
     # Configuration for the application, engines, and railties goes here.
     #
