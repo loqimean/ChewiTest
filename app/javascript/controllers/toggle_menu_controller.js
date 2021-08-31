d44142cf5ce0a9486c80141f9ee87ab764531107
Ivan <vanuha277@gmail.com>
2021-08-31 13:06:15 +0300

    fix(toggle_menu_controller.js) restore file after bad merge

--- app/javascript/controllers/toggle_menu_controller.js
+++ app/javascript/controllers/toggle_menu_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from 'stimulus'
 
 const LIMIT_SHOW_VALUE = 5
-const HIDE_ELEMENTS_STATUS = true
-const SHOW_ELEMENTS_STATUS = false
 
 export default class extends Controller {
   static targets = [ "item", "toggleControl" ]
@@ -14,20 +12,14 @@ export default class extends Controller {
 
   connect() {
     if (this.itemTargets.length > LIMIT_SHOW_VALUE){
-      this.drawItems(HIDDEN_ELEMENTS_STATUS)
+      this.drawItems()
     } else {
-      this.moreControlTarget.hidden = HIDDEN_ELEMENTS_STATUS
-      this.lessControlTarget.hidden = HIDDEN_ELEMENTS_STATUS
+      this.toggleControlTarget.hidden = true
     }
   }
 
-  showMore() {
-    this.drawItems(SHOW_ELEMENTS_STATUS)
-  }
-
-  showLess() {
-    this.drawItems(HIDDEN_ELEMENTS_STATUS)
-  }
+  drawItems() {
+    let state = this.visibilityValue
 
     this.itemTargets.forEach((element, index) => {
       if (index >= LIMIT_SHOW_VALUE) {
