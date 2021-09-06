290e0d1e631a8276ad8df7717fdbddc56c76c678
Hunter2065 <vanuha277@gmail.com>
2021-09-06 13:38:06 +0300

    chore(views|controllers) some changes

--- app/frontend/controllers/modal_controller.js
+++ app/frontend/controllers/modal_controller.js
@@ -1,17 +1,24 @@
 import { Controller } from "stimulus";
 import { Modal } from 'bootstrap';
 
-// let modal = Modal(document.getElementById('myModal'), {
+// let modal = new Modal(document.getElementById('myModal'), {
 //   keyboard: true,
 //   backdrop: true
-// })
+// });
 
 export default class extends Controller {
   static targets = [ 'seeLink' ];
 
+  connect() {
+    let modal = new Modal(document.getElementById('myModal'));
+    modal.show();
+    console.log(this.element);
+  }
+
   open() {
+    console.log('hello');
     // let modal = new Modal(document.getElementById('myModal'), { backdrop: true});
-    console.log(document.getElementById('myModal'));
+    // console.log(document.getElementById('myModal'));
     // let backdropDiv = document.createElement('div');
     // let modalWindow = document.getElementsByClassName('modal')[0];
     // let modalBackgroundCount = document.getElementsByClassName('modal-backdrop').length;
@@ -31,8 +38,8 @@ export default class extends Controller {
     // document.body.classList.remove("modal-open");
     // this.element.removeAttribute("style");
     // this.element.classList.remove("show");
-    // [...document.getElementsByClassName("modal-backdrop")].forEach(div => {
-    //   div.remove();
-    // });
+    [...document.getElementsByClassName("modal-backdrop")].forEach(div => {
+      div.remove();
+    });
   }
 }
