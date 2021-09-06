db986635b6c83c6df63957734fc284a4eb7963a7
Hunter2065 <vanuha277@gmail.com>
2021-09-06 17:39:59 +0300

    chore(modal_controllers.js) remove trash

--- app/frontend/controllers/modal_controller.js
+++ app/frontend/controllers/modal_controller.js
@@ -1,43 +1,20 @@
 import { Controller } from "stimulus";
 import { Modal } from 'bootstrap';
 
-// let modal = new Modal(document.getElementById('myModal'), {
-//   keyboard: true,
-//   backdrop: true
-// });
-
 export default class extends Controller {
-  static targets = [ 'seeLink' ];
+  static values = { userLink: String };
 
   connect() {
     let modal = new Modal(document.getElementById('myModal'));
-    modal.show();
-    console.log(this.element);
-  }
+    let userLink = document.getElementById(`user-link-${this.userLinkValue}`);
 
-  open() {
-    console.log('hello');
-    // let modal = new Modal(document.getElementById('myModal'), { backdrop: true});
-    // console.log(document.getElementById('myModal'));
-    // let backdropDiv = document.createElement('div');
-    // let modalWindow = document.getElementsByClassName('modal')[0];
-    // let modalBackgroundCount = document.getElementsByClassName('modal-backdrop').length;
-    //
-    // if (modalWindow) {
-    //   document.body.classList.add("modal-open");
-    //   modalWindow.setAttribute("style", "display: block;");
-    //   modalWindow.classList.add("show");
-    //
-    //   backdropDiv.classList.add('modal-backdrop', 'fade', 'show');
-    //   document.body.append(backdropDiv);
-    // }
+    modal.show();
+    console.log(this.userLinkValue);
+    userLink.setAttribute('data-bs-toggle', 'modal');
+    userLink.setAttribute('data-bs-target', '#myModal');
   }
 
   close() {
-    console.log('bye');
-    // document.body.classList.remove("modal-open");
-    // this.element.removeAttribute("style");
-    // this.element.classList.remove("show");
     [...document.getElementsByClassName("modal-backdrop")].forEach(div => {
       div.remove();
     });
