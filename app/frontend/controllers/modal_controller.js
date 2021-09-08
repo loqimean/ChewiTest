5c164a1ff933811b7b52a8e264399d732336e1bf
Hunter2065 <vanuha277@gmail.com>
2021-09-08 17:55:20 +0300

    chore(views|js-controllers) remove trash

--- app/frontend/controllers/modal_controller.js
+++ app/frontend/controllers/modal_controller.js
@@ -2,15 +2,11 @@ import { Controller } from "stimulus";
 import { Modal } from 'bootstrap';
 
 export default class extends Controller {
-  static values = { userLink: String };
 
   connect() {
     this.modal = new Modal(document.getElementById('myModal'));
-    let userLink = document.getElementById(`user-link-${this.userLinkValue}`);
 
     this.modal.show();
-    // userLink.setAttribute('data-bs-toggle', 'modal');
-    // userLink.setAttribute('data-bs-target', '#myModal');
   }
 
   disconnect() {
