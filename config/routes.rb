4a4a4316f2b536c13f22060f017132a5104480f9
Hunter2065 <vanuha277@gmail.com>
2021-12-23 12:28:16 +0200

    style(routes.rb) move root path to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   resources :cities
   resources :users do
     collection do
       post :search, to: "users#search"
     end
   end
-  root 'users#index'
 end
