e7e3f269161f158843c49ee72a1d0f521a3364d2
Hunter2065 <vanuha277@gmail.com>
2021-12-23 17:49:26 +0200

    use hidden action route to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :people
-
   resources :cities
   resources :users do
     collection do
       post :search, to: "users#search"
     end
   end
+
   root 'users#index'
+
+  get ':id', to: 'people#show', controller: :people
 end
