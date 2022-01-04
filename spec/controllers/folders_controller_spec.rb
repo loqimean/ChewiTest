1e32c3e91277b433cf3652015f8d257f4e47362c
Hunter2065 <vanuha277@gmail.com>
2022-01-04 15:26:28 +0200

    spec(controllers) add new for TDD

---
+++ spec/controllers/folders_controller_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+# we watch by the directory and if we create folder, then send request for it
+# than if we create a file in some folder, then create only file and foun directory
+
+RSpec.describe FoldersController, type: :controller do
+  context 'POST #create' do
+    context 'when folder is root' do
+      let(:find_folder) { Folder.find_by_name('other') }
+      let(:folder_params_in_root) { { name: 'other' } }
+
+      it 'should be successful' do
+        expect do
+          post :create, params: { folder: folder_params_in_root }
+        end.to change(Folder, :count).by(1)
+      end
+
+      it 'should create a new folder' do
+        post :create, params: { folder: folder_params_in_root }
+
+        expect(find_folder).not_to be_nil
+        expect(find_folder.name).to eq(folder_params_in_root[:name])
+        expect(find_folder.folder_id).to be_nil
+      end
+    end
+
+    context 'when creates in' do
+      let!(:first_exists_folder) { Folder.create(name: 'other') }
+
+      context 'one exists folder' do
+        # With the element in the end
+        # Do we need it in relative_path?
+        let(:folder_params) { { name: 'other', relative_path: 'other/other' } }
+        let(:find_folder) { Folder.find_by(name: 'other', Folder_id: first_exists_folder.id) }
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { folder: folder_params }
+          end.to change(Folder, :count).by(1)
+        end
+
+        it 'should create a new folder' do
+          post :create, params: { folder: folder_params }
+
+          expect(find_folder).not_to be_nil
+          expect(find_folder.name).to eq(folder_params[:name])
+          expect(find_folder.folder_id).to eq(first_exists_folder.id)
+        end
+      end
+
+      context 'two exists folder' do
+        let!(:second_exists_folder) { Folder.create(name: 'other', folder_id: first_exists_folder.id) }
+        let(:folder_params) { { name: 'other', relative_path: 'other/other/other' } }
+        let(:find_folder) { Folder.find_by(name: 'other', Folder_id: second_exists_folder.id) }
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { folder: folder_params }
+          end.to change(Folder, :count).by(1)
+        end
+
+        it 'should create a new folder' do
+          post :create, params: { folder: folder_params }
+
+          expect(find_folder).not_to be_nil
+          expect(find_folder.name).to eq(folder_params[:name])
+          expect(find_folder.folder_id).to eq(second_exists_folder.id)
+        end
+      end
+
+      context 'three exists folder' do
+        let!(:second_exists_folder) { Folder.create(name: 'other', folder_id: first_exists_folder.id) }
+        let!(:third_exists_folder) { Folder.create(name: 'other', folder_id: second_exists_folder.id) }
+        let(:folder_params) { { name: 'other', relative_path: 'other/other/other/other' } }
+        let(:find_folder) { Folder.find_by(name: 'other', Folder_id: third_exists_folder.id) }
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { folder: folder_params }
+          end.to change(Folder, :count).by(1)
+        end
+
+        it 'should create a new folder' do
+          post :create, params: { folder: folder_params }
+
+          expect(find_folder).not_to be_nil
+          expect(find_folder.name).to eq(folder_params[:name])
+          expect(find_folder.folder_id).to eq(third_exists_folder.id)
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,118 @@
+require 'rails_helper'
+
+# we watch by the directory and if we create folder, then send request for it
+# than if we create a file in some folder, then create only file and foun directory
+
+RSpec.describe ItemsController, type: :controller do
+  context 'POST #create' do
+    let(:find_item) { Item.find_by_file_name('file.txt') }
+
+    context 'when folder is root' do
+      let(:item_params_in_root) do
+        {
+          name: 'file.txt',
+          attachment: File.open("#{Rails.root}/spec/files/test.txt")
+        }
+      end
+
+      it 'should be successful' do
+        expect do
+          post :create, params: { file: item_params_in_root }
+        end.to change(Item, :count).by(1)
+      end
+
+      it 'should create a new item' do
+        post :create, params: { file: item_params_in_root }
+
+        expect(find_item).not_to be_nil
+        expect(find_item.name).to eq(item_params_in_root[:name])
+        expect(find_item.folder_id).to be_nil
+      end
+    end
+
+    context 'when creates in' do
+      let!(:first_exists_folder) { Folder.create(name: 'other') }
+
+      context 'one exists folder' do
+        let(:item_params_in_one_folder) do
+          {
+            name: 'file.txt',
+            attachment: File.open("#{Rails.root}/spec/files/test.txt"),
+            relative_path: 'other/file.txt'
+          }
+        end
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { file: item_params_in_one_folder }
+          end.to change(Item, :count).by(1)
+        end
+
+        it 'should create a new item' do
+          post :create, params: { file: item_params_in_one_folder }
+
+          expect(find_item).not_to be_nil
+          expect(find_item.name).to eq(item_params_in_one_folder[:name])
+          expect(find_item.folder_id).to eq(first_exists_folder.id)
+        end
+      end
+
+      context 'two exists folders' do
+        let!(:second_exists_folder) do
+          Folder.create(name: 'other', folder_id: first_exists_folder.id)
+        end
+        let(:item_params_in_one_folder) do
+          {
+            name: 'file.txt',
+            attachment: File.open("#{Rails.root}/spec/files/test.txt"),
+            relative_path: 'other/other/file.txt'
+          }
+        end
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { file: item_params_in_one_folder }
+          end.to change(Item, :count).by(1)
+        end
+
+        it 'should create a new item' do
+          post :create, params: { file: item_params_in_one_folder }
+
+          expect(find_item).not_to be_nil
+          expect(find_item.name).to eq(item_params_in_one_folder[:name])
+          expect(find_item.folder_id).to eq(second_exists_folder.id)
+        end
+      end
+
+      context 'three exists folders' do
+        let!(:second_exists_folder) do
+          Folder.create(name: 'other', folder_id: first_exists_folder.id)
+        end
+        let!(:third_exists_folder) do
+          Folder.create(name: 'other', folder_id: second_exists_folder.id)
+        end
+        let(:item_params_in_one_folder) do
+          {
+            name: 'file.txt',
+            attachment: File.open("#{Rails.root}/spec/files/test.txt"),
+            relative_path: 'other/other/other/file.txt'
+          }
+        end
+
+        it 'should be successful' do
+          expect do
+            post :create, params: { file: item_params_in_one_folder }
+          end.to change(Item, :count).by(1)
+        end
+
+        it 'should create a new item' do
+          post :create, params: { file: item_params_in_one_folder }
+
+          expect(find_item).not_to be_nil
+          expect(find_item.name).to eq(item_params_in_one_folder[:name])
+          expect(find_item.folder_id).to eq(third_exists_folder.id)
+        end
+      end
+    end
+  end
+end
