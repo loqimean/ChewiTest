4c2f0f6d43d5063f158f170ebaa2035c6827c716
Hunter2065 <vanuha277@gmail.com>
2022-01-11 17:28:02 +0200

    spec(controllers) TDD add new for destroying folder

--- spec/controllers/api/v1/virtual_drives_controller_spec.rb
+++ spec/controllers/api/v1/virtual_drives_controller_spec.rb
@@ -31,13 +31,48 @@ RSpec.describe Api::V1::VirtualDrivesController, type: :controller do
       expect(file.folder).to eq(Folder.find_by_name('e'))
     end
 
-    it 'should be successful' do
+    it 'should be successful for DIRECTORY' do
       post :create, params: {
         type: 'DIRECTORY',
         relative_path: 'a/b/c'
       }
 
       expect(response).to be_successful
+    end
+
+    it 'should be successful for FILE' do
+      post :create, params: {
+        type: 'FILE',
+        relative_path: 'a/b/c/file.txt',
+        attachment: Rack::Test::UploadedFile.new("#{Rails.root}/spec/files/test.txt")
+      }
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe '#destroy (DELETE)' do
+    let(:path_to_folder) { 'path/to/folder' }
+    let(:path_to_folder_with_child) { 'path/to/folder/child' }
+
+    context 'when type DIRECTORY' do
+      it 'should be successful for folder' do
+        folder_id = Folder.find_or_create_folder_by_names(path_to_folder)
+
+        delete :destroy, params: { relative_path: 'path/to/folder' }
+
+        expect(Folder.find_by(id: folder_id)).to be_nil
+      end
+
+      it 'should remove children' do
+        child_id = Folder.find_or_create_folder_by_names(path_to_folder_with_child)
+        parent_id = Folder.find_or_create_folder_by_names(path_to_folder)
+
+        delete :destroy, params: { relative_path: 'path/to/folder' }
+
+        expect(Folder.find_by(id: parent_id)).to be_nil
+        expect(Folder.find_by(id: child_id)).to be_nil
+      end
     end
   end
 end
