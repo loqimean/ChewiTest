956b2f5beee21eb3933e561727a2f14e579217f6
Hunter2065 <vanuha277@gmail.com>
2022-01-11 11:23:24 +0200

    spec(controllers) add new and update old

--- spec/controllers/api/v1/virtual_drives_controller_spec.rb
+++ spec/controllers/api/v1/virtual_drives_controller_spec.rb
@@ -2,15 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::VirtualDrivesController, type: :controller do
   describe '#create (POST)' do
-    it 'should create' do
+    it 'should create folder' do
       post :create, params: {
-        type: 'FOLDER',
+        type: 'DIRECTORY',
         relative_path: 'a/b/c'
-      }
-      post :create, params: {
-        type: 'FILE',
-        relative_path: 'd/e/test.txt',
-        attachment: Rack::Test::UploadedFile.new("#{Rails.root}/spec/files/test.txt")
       }
 
       c_folder = Folder.find_by_name('c')
@@ -21,6 +16,14 @@ RSpec.describe Api::V1::VirtualDrivesController, type: :controller do
 
       expect(c_folder.parent).to eq(Folder.find_by_name('b'))
       expect(a_folder.children).to eq(Folder.where(name: 'b'))
+    end
+
+    it 'should create file' do
+      post :create, params: {
+        type: 'FILE',
+        relative_path: 'd/e/test.txt',
+        attachment: Rack::Test::UploadedFile.new("#{Rails.root}/spec/files/test.txt")
+      }
 
       file = Item.find_by_name('test.txt')
 
@@ -30,7 +33,7 @@ RSpec.describe Api::V1::VirtualDrivesController, type: :controller do
 
     it 'should be successful' do
       post :create, params: {
-        type: 'FOLDER',
+        type: 'DIRECTORY',
         relative_path: 'a/b/c'
       }
 
